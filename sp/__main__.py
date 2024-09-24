7612a495ad6ab003f3227cae4cefe0068d3eb5cb
Christopher Boyle <christopher.boyle.101@strath.ac.uk>
2024-09-24 16:28:13 +0100

    Add plot, checkpoint and deployment callbacks, also validate

--- sp/__main__.py
+++ sp/__main__.py
@@ -1,13 +1,52 @@
 from typing import List
 from argparse import ArgumentParser
+from pathlib import Path
 
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 import lightning as L
 from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.callbacks import ModelCheckpoint, Callback
 
 from .data import StockData
 from .models import MODELS
+
+
+class DeploymentCallback(Callback):
+
+    def on_fit_end(self, trainer, model):
+        path = Path(trainer.log_dir) / 'final_model.pt'
+        ts = model.to_torchscript()
+        torch.jit.save(ts, str(path))
+
+
+class PlotCallback(Callback):
+
+    def on_fit_end(self, trainer, model):
+        log_dir = Path(trainer.log_dir)
+        metrics_path = log_dir / 'metrics.csv'
+        df = pd.read_csv(metrics_path)
+
+        steps = np.array(df['step'])
+        train_loss = np.array(df['train_loss'])
+        valid_loss = np.array(df['valid_loss'])
+        train_steps = steps[np.isfinite(train_loss)]
+        train_loss = train_loss[np.isfinite(train_loss)]
+        valid_steps = steps[np.isfinite(valid_loss)]
+        valid_loss = valid_loss[np.isfinite(valid_loss)]
+
+        plt.figure()
+        plt.plot(train_steps, train_loss, label='training')
+        plt.plot(valid_steps, valid_loss, label='validation')
+        plt.xlabel('Step [#]')
+        plt.ylabel('Loss [-]')
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(log_dir / 'loss_plot.png')
+
 
 
 def list_of_str(s: str) -> List[str]:
@@ -26,12 +65,17 @@ def main():
     args = parse_args()
     torch.set_float32_matmul_precision('high')
     data = StockData(context=args.context, foresight=args.foresight)
-    dl = DataLoader(data, batch_size=8, shuffle=True, drop_last=True)
-    trainer = L.Trainer(logger=CSVLogger('.', name='runs'))
+
+    pivot = int(len(data) * 0.8)
+    train_data, valid_data = torch.utils.data.random_split(data, [pivot, len(data) - pivot])
+    train_dl = DataLoader(train_data, batch_size=8, shuffle=True, drop_last=True)
+    valid_dl = DataLoader(valid_data, batch_size=8, shuffle=True, drop_last=True)
     for model_name in args.models:
         model = MODELS[model_name](context=args.context, foresight=args.foresight)
-        trainer.fit(model=model, train_dataloaders=dl)
+        trainer = L.Trainer(logger=CSVLogger('.', name='runs'), callbacks=[DeploymentCallback(), ModelCheckpoint(monitor='valid_loss', every_n_epochs=100), PlotCallback()], max_epochs=10)
+        trainer.fit(model=model, train_dataloaders=train_dl, val_dataloaders=valid_dl)
+    return trainer
 
 
 if __name__ == '__main__':
-    main()
+    trainer = main()
--- sp/models/lstm.py
+++ sp/models/lstm.py
@@ -9,13 +9,25 @@ class LSTMBasedStockPred(L.LightningModule):
         super().__init__()
         self.lstm = nn.LSTM(context, context)
         self.decoder = nn.Linear(context, foresight)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1, _ = self.lstm(x)
+        y_hat = self.decoder(x1)
+        return y_hat
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        x1, _ = self.lstm(x)
-        y_hat = self.decoder(x1)
+        y_hat = self(x)
         loss = nn.functional.mse_loss(y_hat, y)
-        self.log('loss', loss, prog_bar=True)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = nn.functional.mse_loss(y_hat, y)
+        self.log('valid_loss', loss, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
